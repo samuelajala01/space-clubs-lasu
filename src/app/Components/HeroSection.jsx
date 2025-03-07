2020b0669a0830732dfed10018c9795c8b634ef2
Samuel <samuelajala01@gmail.com>
2025-03-07 23:42:53 +0100

    feat: update HeroSection with logo and enhance styling in globals.css

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+import Logo from "../../../public/images/space-clubs-logo.jpg";
+
 const HeroSection = () => {
   return (
     <>
       <>
         <section className="mx-[4vw] text-center mt-[35vh]">
-          <h1 className="text-5xl font-semibold text-white">
-            Space Clubs LASU
-          </h1>
-          <p className="4xl text-white">Insipre Educate Engage</p>
+          
+    <div className="flex justify-center ">
+<Image className="w-[40vw]" src={Logo} alt="Logo" width={100} height={100}/>
+
+    </div>
+          <p className="text-2xl mb-8 text-white">
+            Where curiosity meets the cosmos
+          </p>
+          <p className="text-4xl text-white">Insipre | Educate | Engage</p>
         </section>
       </>
     </>
