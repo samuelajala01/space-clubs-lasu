225630c10b18367d6e75522ef202e067e64bbfad
Samuel <samuelajala01@gmail.com>
2025-03-15 18:02:31 +0100

    chore: add contact to footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -3,6 +3,12 @@ const Footer = () => {
     <>
       <Footer>
         <div>this is the footer</div>
+        <div>LOGO</div>
+        <p>About</p>
+        <p>Reach Out</p>
+        <p>Partner with us</p>
+        <p>Donate</p>
+        <p>Space Clubs 2025, All rights reserved</p>
       </Footer>
     </>
   );
